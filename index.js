aa38a0ab347cf11ab0280938a7b8dbcda169ed36
Andrew Gonzalez <drewgonz974@gmail.com>
2022-04-25 18:30:11 -0700

    More cases being handled

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
 //on page load
 $(document).ready(function () {
+    //initialize state
     const currentInput = $('#current-input');
-    currentInput.text(1);
-    
+    const prevInputs = $('#prev-inputs');
+    let equation = [''];
+
+
     //inputs
     $('.operation').on('click', function () {
-        console.log($(this).text());
+        updateInputDisplay($(this).text());
     })
     $('.number').on('click', function () {
-        console.log(JSON.parse($(this).text()));
+        updateInputDisplay($(this).text());
     })
     $(document).on('keypress', function (event) {
         //fun fact: apparently converting an empty string into a number returns 0 instead of NaN
-        if(!isNaN(Number(event.key)) && event.key !== ' ') {
-            console.log('is a number');
+        if (!isNaN(Number(event.key)) && event.key !== ' ') {
+            updateInputDisplay(event.key);
+        }
+        switch (event.key) {
+            case '=':
+                calculateEquation();
+                break;
+            case '+':
+                console.log('add');
+                break;
+            case '-':
+                console.log('subtract');
+                break;
+            case '*':
+                console.log('multiply');
+                break;
+            case '/':
+                console.log('divide');
+                break;
+            case 'Enter':
+                calculateEquation();
+                break;
+            default:
+                break;
         }
     })
 
-    //handler
-    function buttonHandler () {
+    //handle and update display
+    function updateInputDisplay(input) {
+        switch (input) {
+            case 'Del':
+                equation[equation.length - 1] = equation[equation.length - 1].slice(0, -1);
+                currentInput.text(equation);
+                break;
+            case 'Clr':
+                //reset state and clear display
+                equation = [''];
+                currentInput.html('&nbsp;');
+                break;
+            default:
+                //update the display normally
+                equation[equation.length - 1] += input;
+                currentInput.text(equation);
+                break;
 
+        }
+    }
+    function calculateEquation() {
+        console.log('calculate');
     }
 })
