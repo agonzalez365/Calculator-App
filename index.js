3582455a37d99fd7cfba4a91093bd7a33a2ca560
Andrew Gonzalez <drewgonz974@gmail.com>
2022-04-27 14:35:01 -0700

    Redid functionality using handlers instead one massive inefficient function. Still need to reimplement clickable buttons

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     //initialize state
     const currentInput = $('#current-input');
     const prevInputs = $('#prev-inputs');
-    let equation = [''];
+    let equation = [];
 
 
     //inputs
@@ -14,55 +14,119 @@ $(document).ready(function () {
         updateInputDisplay($(this).text());
     })
     $(document).on('keypress', function (event) {
+        //looks kinda funky but this will check if a number is pressed, or if an operator is pressed
+        //it will then call the related function that handles the input
         //fun fact: apparently converting an empty string into a number returns 0 instead of NaN
         if (!isNaN(Number(event.key)) && event.key !== ' ') {
-            updateInputDisplay(event.key);
+            handleNumber(event.key);
         }
         switch (event.key) {
-            case '=':
-                calculateEquation();
-                break;
             case '+':
-                console.log('add');
+                handleOperator(event.key);
                 break;
             case '-':
-                console.log('subtract');
+                handleOperator(event.key);
                 break;
             case '*':
-                console.log('multiply');
+                handleOperator(event.key);
                 break;
             case '/':
-                console.log('divide');
+                handleOperator(event.key);
+                break;
+            case '=':
+                handleCalculation();
                 break;
             case 'Enter':
-                calculateEquation();
+                handleCalculation();
                 break;
+            case 'c':
+                handleClear();
+                break;
+            case 'd':
+                handleDeletion();
+                break;
+
             default:
                 break;
         }
     })
 
-    //handle and update display
-    function updateInputDisplay(input) {
-        switch (input) {
-            case 'Del':
-                equation[equation.length - 1] = equation[equation.length - 1].slice(0, -1);
-                currentInput.text(equation);
-                break;
-            case 'Clr':
-                //reset state and clear display
-                equation = [''];
-                currentInput.text(equation);
-                break;
-            default:
-                //update the display normally
-                equation[equation.length - 1] += input;
-                currentInput.text(equation);
-                break;
-
+    function handleOperator(operator){
+        //if equation isn't empty, proceed
+        if(equation.length > 0){
+            //if previous input is already an operator, replace the operator
+            if(isNaN(Number(equation[equation.length - 1]))) {
+                equation[equation.length - 1] = operator;
+            }
+            //else just add the operator to the array
+            else {
+                equation.push(operator);
+            }
+            updateScreen(false);
         }
     }
-    function calculateEquation() {
+
+    function handleNumber(number){
+        console.log('number');
+        //if equation is not empty, proceed
+        if(equation.length > 0) {
+            //if last element in array is an operator, add the number as a new element
+            if(isNaN(Number(equation[equation.length - 1]))){
+                equation.push(number);
+            }
+            //else just append the number to the existing number
+            else {
+                equation[equation.length - 1] += number;
+            }
+        }
+        //if equation is empty
+        else {
+            equation.push(number);
+        }
+        updateScreen(false);
+    }
+
+    function handleDeletion(){
+        //if equation isn't empty, proceed, otherwise do nothing
+        if(equation.length > 0){
+            //if the last element is an operator remove it
+            if(isNaN(Number(equation[equation.length - 1]))){
+                equation.pop();
+            }
+            //if the last element is a number, remove the last number
+            else {
+                equation[equation.length - 1] = equation[equation.length - 1].slice(0, -1);
+                if(equation[0] === ''){
+                    equation = [];
+                }
+            }
+            updateScreen();
+        }
+    }
+
+    function handleClear(){
+        //reset equation
+        equation = [];
+        updateScreen(false);
+    }
+
+
+    function handleCalculation() {
         console.log('calculate');
+    }
+
+
+    //TO DO: check if previous element is operation before pushing to array
+    //TO DO: Limit input size
+    //handle and update display
+    function updateScreen(calculate) {
+        console.log(equation);
+        if(!calculate){+
+            currentInput.text(equation.join(' '));
+        }
+        else {
+            prevInputs.text(equation.join(' '));
+            currentInput.text('something');
+        }
     }
 })
