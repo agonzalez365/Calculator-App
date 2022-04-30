388cc796013054607f9095c3db3e15da5b994932
Andrew Gonzalez <drewgonz974@gmail.com>
2022-04-29 17:38:25 -0700

    Improved formatting

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     //clickable inputs
     $('.operation').on('click', function () {
         const id = $(this).attr('id');
-        switch($(this).attr('id')){
+        switch ($(this).attr('id')) {
             case '+':
                 handleOperator(id);
                 break;
@@ -71,17 +71,16 @@ $(document).ready(function () {
             case 'd':
                 handleDeletion();
                 break;
-
             default:
                 break;
         }
     })
 
-    function handleOperator(operator){
+    function handleOperator(operator) {
         //if equation isn't empty, proceed
-        if(equation.length > 0){
+        if (equation.length > 0) {
             //if previous input is already an operator, replace the operator
-            if(isNaN(Number(equation[equation.length - 1]))) {
+            if (isNaN(Number(equation[equation.length - 1]))) {
                 equation[equation.length - 1] = operator;
             }
             //else just add the operator to the array
@@ -92,11 +91,11 @@ $(document).ready(function () {
         }
     }
 
-    function handleNumber(number){
+    function handleNumber(number) {
         //if equation is not empty, proceed
-        if(equation.length > 0) {
+        if (equation.length > 0) {
             //if last element in array is an operator, add the number as a new element
-            if(isNaN(Number(equation[equation.length - 1]))){
+            if (isNaN(Number(equation[equation.length - 1]))) {
                 equation.push(number);
             }
             //else just append the number to the existing number
@@ -112,10 +111,10 @@ $(document).ready(function () {
     }
 
     function handleDecimalPoint() {
-        if(equation.length > 0){
+        if (equation.length > 0) {
             //if last element is check if it already has a
-            if(!isNaN(Number(equation[equation.length - 1]))){
-                if(!equation[equation.length - 1].includes('.')){
+            if (!isNaN(Number(equation[equation.length - 1]))) {
+                if (!equation[equation.length - 1].includes('.')) {
                     equation[equation.length - 1] += '.';
                     updateScreen([]);
                 }
@@ -131,18 +130,18 @@ $(document).ready(function () {
         }
     }
 
-    function handleDeletion(){
+    function handleDeletion() {
         //if equation isn't empty, proceed, otherwise do nothing
-        if(equation.length > 0){
+        if (equation.length > 0) {
             //if the last element is an operator remove it
-            if(isNaN(Number(equation[equation.length - 1]))){
+            if (isNaN(Number(equation[equation.length - 1]))) {
                 equation.pop();
             }
             //if the last element is a number, remove the last number
             else {
                 equation[equation.length - 1] = equation[equation.length - 1].slice(0, -1);
                 //if element ends up empty, remove it from the array
-                if(equation[equation.length - 1] === ''){
+                if (equation[equation.length - 1] === '') {
                     equation.pop();
                 }
             }
@@ -150,7 +149,7 @@ $(document).ready(function () {
         }
     }
 
-    function handleClear(){
+    function handleClear() {
         //reset equation
         equation = [];
         updateScreen([]);
@@ -166,33 +165,33 @@ $(document).ready(function () {
         //probably a way to do this without multiple loops
         //but this is my scuffed solution to implement functioning order of operations
         //multiplication/division pass
-        for(let i = 1; i < equationArray.length; i += 2){
-            if(i <= equationArray.length) {
-                if(equationArray[i] === '*' || equationArray[i] === '/'){
+        for (let i = 1; i < equationArray.length; i += 2) {
+            if (i <= equationArray.length) {
+                if (equationArray[i] === '*' || equationArray[i] === '/') {
                     console.log(equationArray[i]);
                     //operate on values next to operator
                     //assign result to element on the right so the iterations can continue
                     equationArray[i + 1] = operationEvaluator(Number(equationArray[i - 1]), Number(equationArray[i + 1]), equationArray[i]);
                     //mark for removal
                     equationArray[i - 1] = 'd';
-                    equationArray[i] =  'd';
+                    equationArray[i] = 'd';
                 }
             }
         }
-        
+
         //filter before next pass
         equationArray = filterArray(equationArray);
         console.log(equationArray);
 
         //addition/subtraction pass
-        for(let i = 1; i < equationArray.length; i += 2){
-            if(i <= equationArray.length) {
-                if(equationArray[i] === '+' || equationArray[i] === '-'){
+        for (let i = 1; i < equationArray.length; i += 2) {
+            if (i <= equationArray.length) {
+                if (equationArray[i] === '+' || equationArray[i] === '-') {
                     console.log(equationArray[i]);
                     equationArray[i + 1] = operationEvaluator(Number(equationArray[i - 1]), Number(equationArray[i + 1]), equationArray[i]);
                     //mark for removal
                     equationArray[i - 1] = 'd';
-                    equationArray[i] =  'd';
+                    equationArray[i] = 'd';
                 }
             }
         }
@@ -204,9 +203,9 @@ $(document).ready(function () {
 
     }
 
-    function filterArray(array){
+    function filterArray(array) {
         array = array.filter(function (element) {
-            if(element !== 'd'){
+            if (element !== 'd') {
                 return true;
             }
             else {
@@ -218,9 +217,9 @@ $(document).ready(function () {
 
     //use with handle calculation to evaluate parts of the expression
     function operationEvaluator(val1, val2, operation) {
-        if(typeof operation === 'string'){
+        if (typeof operation === 'string') {
             let result;
-            switch(operation){
+            switch (operation) {
                 case '+':
                     result = val1 + val2;
                     break;
@@ -241,13 +240,13 @@ $(document).ready(function () {
         else {
             console.log('what happened');
         }
-        
+
     }
 
     //TO DO: Limit input size
     //handle and update display
     function updateScreen(array) {
-        if(array.length === 0){
+        if (array.length === 0) {
             currentInput.text(equation.join(' '));
         }
         else {
